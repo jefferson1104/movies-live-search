dace4916251cd98524bce9486630efabe2dbc05c
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2025-06-05 10:52:30 -0300

    chore: installing the tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 function App() {
   // Renders
   return (
-    <>
-      <header>
-        <h1>Movies live search</h1>
+    <div className="min-h-screen flex flex-col">
+      <header className="h-16 flex items-center justify-center bg-gray-100 shadow">
+        <h1 className="text-2xl font-bold">Movies Live Search</h1>
       </header>
 
-      <main>
-        <p>
-          Movies Live Search é uma aplicação que permite buscar filmes em tempo
-          real, exibindo resultados instantâneos enquanto o usuário digita. O
-          projeto utiliza React, TypeScript e APIs públicas de filmes para
-          oferecer uma experiência rápida e intuitiva.
-        </p>
+      <main className="flex flex-grow items-center justify-center mt-16">
+        <div className="max-w-xl">
+          <p className="text-lg text-center">
+            Movies Live Search é uma aplicação que permite buscar filmes em
+            tempo real, exibindo resultados instantâneos enquanto o usuário
+            digita. O projeto utiliza React, TypeScript e APIs públicas de
+            filmes para oferecer uma experiência rápida e intuitiva.
+          </p>
+        </div>
       </main>
 
-      <footer>
-        <p>&copy; 2025 Soares Dev LTDA.</p>
+      <footer className="h-16 flex items-center justify-center bg-gray-100 shadow">
+        <p className="text-sm">&copy; 2025 Soares Dev LTDA.</p>
       </footer>
-    </>
+    </div>
   );
 }
 
