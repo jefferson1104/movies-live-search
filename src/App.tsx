310fb2a59ea19a8fe7af6c1912362a9c1492a1ed
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2025-06-07 11:59:25 -0300

    feat: implementing tmdb api to movie search

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,61 @@
-import { InputSearch } from "./components/InputSearch";
+import { useEffect, useRef, useState } from "react";
+
+import api from "./server/api";
+
+import { MovieInputSearch } from "./components/MovieInputSearch";
 
 function App() {
+  // States
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  // Constants
+  const debounceTimeoutRef = useRef<number | undefined>(undefined);
+
+  // Methods
+  const handleChange = (value: string) => {
+    setQuery(value);
+
+    // Limpa timeout anterior, se houver
+    if (debounceTimeoutRef.current) {
+      clearTimeout(debounceTimeoutRef.current);
+    }
+
+    // Cria um novo timeout
+    debounceTimeoutRef.current = window.setTimeout(() => {
+      setDebouncedQuery(value);
+    }, 500);
+  };
+
+  const fetchMovies = async (debouncedQuery: string) => {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/search/movie", {
+        params: {
+          query: debouncedQuery,
+        },
+      });
+
+      setResults(response.data.results);
+    } catch (error) {
+      console.error("Erro ao buscar filmes:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Effects
+  useEffect(() => {
+    if (!debouncedQuery) {
+      setResults([]);
+      return;
+    }
+
+    fetchMovies(debouncedQuery);
+  }, [debouncedQuery]);
+
   // Renders
   return (
     <div className="min-h-screen flex flex-col">
@@ -10,7 +65,13 @@ function App() {
 
       <main className="flex flex-col flex-grow items-center">
         <div className="mt-16 mb-8 w-[480px]">
-          <InputSearch />
+          <MovieInputSearch
+            isLoading={isLoading}
+            value={query}
+            searchValue={debouncedQuery}
+            searchResults={results}
+            onSearchChange={handleChange}
+          />
         </div>
 
         <div className="max-w-xl">
--- src/components/MovieInputSearch.tsx
+++ src/components/MovieInputSearch.tsx
@@ -1,72 +1,57 @@
 import { useState, useRef, useEffect } from "react";
 import { FaChevronDown } from "react-icons/fa6";
+import { LuLoaderCircle } from "react-icons/lu";
 
-const mockData = [
-  "Star Wars",
-  "Star Trek",
-  "Stargate",
-  "The Godfather",
-  "The Dark Knight",
-  "Pulp Fiction",
-  "The Lord of the Rings",
-  "The Matrix",
-  "The Shawshank Redemption",
-  "Inception",
-  "Fight Club",
-];
+import type { IMovie } from "../interfaces/movie";
 
-export function InputSearch() {
+interface IInputSearchProps {
+  isLoading?: boolean;
+  value?: string;
+  searchValue?: string;
+  searchResults?: IMovie[];
+  onSearchChange: (value: string) => void;
+}
+
+export function MovieInputSearch({
+  isLoading = false,
+  value = "",
+  searchValue = "",
+  searchResults = [],
+  onSearchChange,
+}: IInputSearchProps) {
   // Hooks
   const wrapperRef = useRef<HTMLDivElement>(null);
   const itemRefs = useRef<(HTMLLIElement | undefined)[]>([]);
 
   // States
-  const [search, setSearch] = useState("");
-  const [filteredResults, setFilteredResults] = useState<string[]>([]);
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   // Methods
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setSearch(value);
-
-    if (value.trim() === "") {
-      setFilteredResults([]);
-    } else {
-      const results = mockData.filter((item) =>
-        item.toLowerCase().includes(value.toLowerCase())
-      );
-      setFilteredResults(results);
-    }
-
-    setActiveIndex(null);
-  };
-
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (filteredResults.length === 0) return;
+    if (searchResults?.length === 0) return;
 
-    if (e.key === "ArrowDown") {
+    if (e.key === "ArrowDown" && searchResults) {
       e.preventDefault();
       setActiveIndex((prev) => {
         const nextIndex =
-          prev === null ? 0 : Math.min(prev + 1, filteredResults.length - 1);
+          prev === null ? 0 : Math.min(prev + 1, searchResults.length - 1);
         return nextIndex;
       });
     }
 
-    if (e.key === "ArrowUp") {
+    if (e.key === "ArrowUp" && searchResults) {
       e.preventDefault();
       setActiveIndex((prev) => {
         const nextIndex =
-          prev === null ? filteredResults.length - 1 : Math.max(prev - 1, 0);
+          prev === null ? searchResults.length - 1 : Math.max(prev - 1, 0);
         return nextIndex;
       });
     }
 
     if (e.key === "Enter" && activeIndex !== null) {
       e.preventDefault();
-      setSearch(filteredResults[activeIndex]);
-      setFilteredResults([]);
+      // setSearch(filteredResults[activeIndex]);
+      // setFilteredResults([]);
       setActiveIndex(null);
     }
   };
@@ -86,32 +71,34 @@ export function InputSearch() {
   return (
     <div className="group" ref={wrapperRef}>
       <p className="text-gray-800">Pesquise um filme</p>
-      <div className="flex justify-between gap-2 w-full h-10 px-2 py-1 rounded-md border border-gray-600 focus-within:border-blue-500">
+      <div className="flex items-center justify-between gap-2 w-full h-10 px-2 py-1 rounded-md border border-gray-600 focus-within:border-blue-500">
         <input
           className="w-full focus:outline-none"
           type="text"
           placeholder="Ex: Star Wars"
-          value={search}
-          onChange={handleSearch}
+          value={value}
+          onChange={(e) => onSearchChange(e.target.value)}
           onKeyDown={handleKeyDown}
         />
 
-        <button>
+        {isLoading && <LuLoaderCircle className="animate-spin text-blue-500" />}
+
+        {!isLoading && (
           <FaChevronDown className="fill-gray-600 group-focus-within:fill-blue-500 transform transition-transform duration-300 group-focus-within:rotate-180" />
-        </button>
+        )}
       </div>
 
-      {search.length > 0 && (
+      {searchValue && (
         <ul className="group mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-40 overflow-y-auto">
-          {filteredResults.length === 0 && (
+          {searchResults?.length === 0 && (
             <div className="flex items-center justify-center h-14">
               <p className="text-center text-gray-400">
-                Nenhum resultado encontrado para "{search}"
+                Nenhum resultado encontrado para "{searchValue}"
               </p>
             </div>
           )}
 
-          {filteredResults.map((item, index) => (
+          {searchResults?.map((item, index) => (
             <li
               className={`px-4 py-2 cursor-pointer ${
                 activeIndex === index ? "bg-gray-200" : "hover:bg-gray-100"
@@ -120,16 +107,16 @@ export function InputSearch() {
               ref={(el) => {
                 itemRefs.current[index] = el ?? undefined;
               }}
-              onClick={() => setSearch(item)}
+              onClick={() => console.log("SELECTED MOVIE", item)}
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
-                  setSearch(item);
-                  setFilteredResults([]);
+                  onSearchChange(item.title);
                   setActiveIndex(null);
+                  console.log("Selected item:", item);
                 }
               }}
             >
-              {item}
+              {item.title}
             </li>
           ))}
         </ul>
